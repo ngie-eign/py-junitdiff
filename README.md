740f78952c515c8fdea2c8638f732eef96f7fa49
Enji Cooper <yaneurabeya@gmail.com>
2026-03-08 21:34:23 -0700

    Finish off basic JUnit diff functionality
    This change implements basic difference functionality of simplified
    representations of JUnitXML objects, leveraging the deepdiff package.
    
    This is a first working iteration of the tool. Additional basic changes
    may be done in the future.
    
    Signed-off-by: Enji Cooper <yaneurabeya@gmail.com>

--- README.md
+++ README.md
@@ -2,4 +2,33 @@
 
 Simple JUnit XML test case report summary tool.
 
+## Installation
 
+### pipenv
+
+```bash
+% pipenv install .
+```
+
+### pipx
+
+```bash
+% pipx install .
+```
+
+## Usage
+
+### Compare `file1.xml` and `file2.xml` at a testcase level
+
+```bash
+% junitdiff file1.xml file2.xml
+```
+
+### Compare `file1.xml` and `file2.xml` at a testsuite level
+
+**Important**: please note that testsuite results might not be expressed by all
+tools, e.g., `kyua report-junit`.
+
+```bash
+% junitdiff --diff-level=testsuite file1.xml file2.xml
+```
--- src/junitdiff/__main__.py
+++ src/junitdiff/__main__.py
@@ -1,56 +1,114 @@
 """JUnitDiff CLI."""
+# SPDX-License-Identifier: BSD-2-Clause
 
 from __future__ import annotations
 
 import argparse
-import logging
+import enum
+import pprint
+import sys
+import traceback
+from dataclasses import dataclass
 
+import deepdiff
+import junitparser
 from junitparser import JUnitXml
 
 # ruff: noqa: T201
 
 
-# import enum
-# class DiffLevel(enum.Enum):
-#    TEST_SUITE = enum.auto()
-#    TEST_CASE = enum.auto()
+class DiffExitCode(enum.IntEnum):
+    """Exit code to return.
+
+    This matches `EXIT CODE` of many other diff-like utilities.
+    """
+
+    NO_DIFF = 0
+    DIFF = 1
+    ERROR = 2
 
 
-def diff_runs_at_testsuite_level(
-    run1: JUnitXml, run2: JUnitXml,
-) -> tuple[set[tuple[str, str], ...]]:
-    run1_results = set()
-    for suite in run1:
-        for case in suite:
-            run1_results.add(((suite.name, case.name), case.result))
-    run2_results = {
-        ((suite.name, case.name), case.result) for suite in run2 for case in suite
-    }
-    return run2_results - run1_results, run1_results - run2_results
+@dataclass
+class FilteredResult:
+    """Provide filtered fields for `junitparser.Element` for deepdiff."""
+
+    name: list[str]
+    errors: int
+    failures: int
+    skipped: int
+    tests: int
 
 
-def diff_runs_at_testcase_level(
-    run1: JUnitXml, run2: JUnitXml,
-) -> tuple[set[tuple[str, str], ...]]:
-    run1_results = set()
-    for suite in run1:
-        print(suite.name)
-        for case in suite:
-            print(case.name)
-            run1_results.add(((suite.name, case.name), case.result))
-    run2_results = {
-        ((suite.name, case.name), case.result) for suite in run2 for case in suite
-    }
-    return run2_results - run1_results, run1_results - run2_results
+def flatten_elements(
+    run_output: JUnitXml,
+    junit_type: junitparser.Element,
+) -> list[junitparser.Element]:
+    """Flatten out elements of a particular type, `junit_type`, from `run`.
+
+    Args:
+        run_output: JUnit XML output.
+        junit_type: `Element` type to parse out, e.g., `junitparser.TestCase`.
+
+    Returns:
+        A list of filtered `Elements` of a selected type.
+
+    """
+    return list(run_output.iterchildren(junit_type))
 
 
-def main(argv: list[str] | None = None) -> None:
-    """Eponymous main."""
-    logging.basicConfig(
-        format="%(filename)s: %(levelname)s: %(message)s", level=logging.INFO,
+def filter_properties(elem: junitparser.Element) -> list[FilteredResult]:
+    """Filter out properties from `elem`.
+
+    Args:
+        elem: a root element to parse results from.
+
+    Returns:
+        A list of filtered results.
+
+    """
+    name: list[str] = []
+    errors = failures = skipped = tests = 0
+    if isinstance(elem, junitparser.TestCase):
+        tests = 1
+        for res in elem.result:
+            if isinstance(res, junitparser.Error):
+                errors += 1
+            elif isinstance(res, junitparser.Failure):
+                failures += 1
+            elif isinstance(res, junitparser.Skipped):
+                skipped += 1
+        name = [elem.name]
+        if elem.classname is None:
+            name.append(elem.classname)
+    elif isinstance(elem, junitparser.TestSuite):
+        name = [elem.name]
+        errors = elem.errors
+        failures = elem.failures
+        skipped = elem.skipped
+        tests = elem.tests
+    else:
+        # ruff: noqa: SLF001
+        err_msg = f"Unsupported type: {elem._tag}"
+        raise NotImplementedError(err_msg)
+
+    return FilteredResult(
+        name,
+        errors,
+        failures,
+        skipped,
+        tests,
     )
+
+
+def _main(argv: list[str] | None = None) -> int:
+    """Eponymous main (inner)."""
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--diff-level", choices=("test-suite", "test-case"))
+    argparser.add_argument(
+        "--diff-level",
+        choices=("testcase", "testsuite"),
+        default="testcase",
+        help="Level to compare from.",
+    )
     argparser.add_argument("run1_xml", type=argparse.FileType("r"))
     argparser.add_argument("run2_xml", type=argparse.FileType("r"))
     args = argparser.parse_args(args=argv)
@@ -58,12 +116,32 @@ def main(argv: list[str] | None = None) -> None:
     run1_xml = JUnitXml.fromfile(args.run1_xml)
     run2_xml = JUnitXml.fromfile(args.run2_xml)
 
-    if args.diff_level == "test-case":
-        diff_between_run2_and_run1_xml = diff_runs_at_testcase_level(run1_xml, run2_xml)
+    if args.diff_level == "testcase":
+        junit_result_type = junitparser.TestCase
     else:
-        diff_between_run2_and_run1_xml = diff_runs_at_testsuite_level(run1_xml, run2_xml)
+        junit_result_type = junitparser.TestSuite
 
-    print(diff_between_run2_and_run1_xml)
+    run1_results = flatten_elements(run1_xml, junit_result_type)
+    run2_results = flatten_elements(run2_xml, junit_result_type)
+
+    res1_filtered = [filter_properties(a) for a in run1_results]
+    res2_filtered = [filter_properties(b) for b in run2_results]
+    differ = deepdiff.DeepDiff(res1_filtered, res2_filtered)
+    if differ:
+        print(f"Differences exist:\n{pprint.pformat(differ)}")
+        return DiffExitCode.DIFF
+    print("No noticeable differences")
+    return DiffExitCode.NO_DIFF
+
+
+def main(argv: ... = None) -> int:
+    """Eponymous main (outer)."""
+    try:
+        sys.exit(_main(argv=argv))
+    # ruff: noqa: BLE001
+    except Exception:
+        traceback.print_exc()
+        sys.exit(DiffExitCode.ERROR)
 
 
 if __name__ == "__main__":
